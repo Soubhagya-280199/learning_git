e396c8a7d3558e675bdd81e135021afa7e7b47f4
Soubhagya-280199 <ssethy.srs@gmail.com>
2022-02-19 23:36:41 +0530

    added test case in sum.py

--- sum.py
+++ sum.py
@@ -2,6 +2,11 @@ class practice:
     def __init__(self,a,b):
         self.a = a
         self.b = b
+        
+        #test case
+        if self.a <= 0 and self.b <= 0:
+            print("values are less than or equals to zero so answer is zero")
+            exit()
 
     def addition(self):
         return self.a + self.b
@@ -9,5 +14,5 @@ class practice:
 
 
 
-obj = practice(2,3)
+obj = practice(0,0)
 print( "sum of two values are ",obj.addition())
