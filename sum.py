ee6dea36e60070834f4632c0508aaf9272b5cace
Soubhagya-280199 <ssethy.srs@gmail.com>
2022-02-19 23:26:06 +0530

    sum.py committed 1st

---
+++ sum.py
@@ -0,0 +1,13 @@
+class practice:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        return self.a + self.b
+
+
+
+
+obj = practice(2,3)
+print( "sum of two values are ",obj.addition())
